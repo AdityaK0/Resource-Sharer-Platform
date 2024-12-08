855aea52117c2e36ecec844e5c0428b37d50eca7
AdityaKO2 <aditya123906@gmail.com>
2024-12-09 01:45:06 +0530

    removed

--- blog/startup.py
+++
@@ -1,9 +0,0 @@
-import os
-from django.core.management import call_command
-
-db_path = os.path.join('/tmp', 'db.sqlite3')
-
-if not os.path.exists(db_path):
-    print("Initializing SQLite database...")
-    call_command('migrate')
-    call_command('loaddata', 'initial_data.json')  # Optional: Load fixtures if needed
